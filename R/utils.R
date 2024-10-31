dd6c50eba9bb45a57bcd731156fbbc9b140dd41b
Lisa DeBruine <debruine@gmail.com>
2024-10-31 14:23:55 +0100

    Added %||% definition for older R versions

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,24 @@
+#' Default value for `NULL`
+#'
+#' This infix function makes it easy to replace `NULL`s with a default
+#' value. It's inspired by the way that Ruby's or operation (`||`)
+#' works.
+#'
+#' @param x,y If `x` is NULL, will return `y`; otherwise returns `x`.
+#' @export
+#' @name op-null-default
+#' @examples
+#' 1 %||% 2
+#' NULL %||% 2
+`%||%` <- function(x, y) {
+  if (is_null(x)) y else x
+}
+
+# Reexport from base on newer versions of R to avoid conflict messages
+if (exists("%||%", envir = baseenv())) {
+  `%||%` <- get("%||%", envir = baseenv())
+}
+
 #' Less scary green messages
 #'
 #' @param ... message components (see \code{\link[base]{message}})
